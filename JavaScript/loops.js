f7a8c0ddaa329641af57621d20ad6568b4b61eea
Bhanutejaswini <209X1A2922@gprec.com>
2024-12-05 13:51:28 +0530

    msg

---
+++ JavaScript/loops.js
@@ -0,0 +1,30 @@
+// for (let i=1;i<=5;i++)
+// {
+//   console.log(i)
+// }
+// while
+
+// let i=1
+// while(i<=5)
+// {
+//   console.log(i)
+//   i++;
+// }
+
+// do while loop
+// let i=1;
+// do{
+//   console.log(i)
+//   i++;
+// }
+
+// for in loop
+
+// let person={ name:"Seetha",age:13, city: "swiss"};
+// for(let key in person)
+// console.log(key +":"+person[key]);
+
+// for of loop
+let arr=[1,2,3]
+for(let value of arr)
+console.log(value);
---
+++ JavaScript/popup.js
@@ -0,0 +1,9 @@
+// alert("have a nice day")
+// 
+// prompt("give your name")
+// 
+// prompt("give your name",bhanu)
+
+// 
+ let a= confirm("you can close")
+ console.log(a);
